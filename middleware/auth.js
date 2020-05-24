935da0615e4c777419ecfaa6bee20e5a8bcad130
SaiChandra2896 <saichandra2896@gmail.com>
2020-05-24 13:41:44 +0530

    add auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,8 +3,19 @@ const config = require('config');
 
 module.exports = function (req, res, next) {
 
+    //get the token
     const token = req.header('x-auth-token');
+    //if no token
     if (!token) {
         return res.status(401).json({ msg: 'No token authorization denied' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, config.get('jwtSecret'));
+        req.user = decoded.user;
+        next();
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Token is not valid');
     }
 }
